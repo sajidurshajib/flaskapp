a6f885a25449452ea6c13f4aa4bccaf8bf1d4b04
Sajidur Rahman <sajidur.inbox@gmail.com>
2020-12-26 15:38:11 +0600

    init

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum nisi, deleniti minima pariatur nemo exercitationem ducimus sed laboriosam dolor, maxime quas aperiam amet tenetur obcaecati beatae eveniet dolorem maiores corrupti!',
+            'author':'Sajidur Rahman',
+            'create_date':'25-12-2020'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum nisi, deleniti minima pariatur nemo exercitationem ducimus sed laboriosam dolor, maxime quas aperiam amet tenetur obcaecati beatae eveniet dolorem maiores corrupti!',
+            'author':'Sajidur Rahman',
+            'create_date':'25-12-2020'
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum nisi, deleniti minima pariatur nemo exercitationem ducimus sed laboriosam dolor, maxime quas aperiam amet tenetur obcaecati beatae eveniet dolorem maiores corrupti!',
+            'author':'Sajidur Rahman',
+            'create_date':'25-12-2020'
+        }
+    ]
+
+    return articles
